27b7fb998d4be143cb8d919ac34a9ec4fb0bc5c5
bhargavreddy1179 <bhargavareddy019@gmail.com>
2024-07-08 19:17:28 +0530

    Added Pages

--- force-app/main/default/lwc/survey/survey.js
+++ force-app/main/default/lwc/survey/survey.js
@@ -1,12 +1,15 @@
 import { LightningElement, track } from 'lwc';
+import createSurvey from '@salesforce/apex/SurveyController.createSurvey';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class Survey extends LightningElement {
-   @track questions = []
-   @track newObj
+   
+   @track pages = [{ questions: [] }]
+   title = ""
    handleAddQuestion(event) {
-      this.questions.push(
+      this.pages[event.target.dataset.page].questions.push(
          {
-            id: this.questions.length + 1,
+            id: this.pages[event.target.dataset.page].questions.length + 1,
             question_text: '',
             inputType: 'text',
             isTextField: true,
@@ -21,6 +24,12 @@ export default class Survey extends LightningElement {
             required: false
          }
       )
+       console.log(pages)
+   }
+
+   handleAddPage(event) {
+      this.pages.push({ questions: [] })
+      console.log(pages)
    }
 
    get options() {
@@ -34,97 +43,147 @@ export default class Survey extends LightningElement {
    }
 
    handleInputTypeChange(event) {
+      const indexPage = event.target.dataset.page
       const index = event.target.dataset.id
-      this.questions[index].inputType = event.target.value
-      this.questions[index].isTextField = event.target.value === 'text'
-      this.questions[index].isNumberField = event.target.value === 'number'
-      this.questions[index].isRadioField = event.target.value === 'radio'
-      this.questions[index].isCheckBoxField = event.target.value === 'checkbox'
-      this.questions[index].isRatingField = event.target.value === 'rating'
+      this.pages[indexPage].questions[index].inputType = event.target.value
+      this.pages[indexPage].questions[index].isTextField = event.target.value === 'text'
+      this.pages[indexPage].questions[index].isNumberField = event.target.value === 'number'
+      this.pages[indexPage].questions[index].isRadioField = event.target.value === 'radio'
+      this.pages[indexPage].questions[index].isCheckBoxField = event.target.value === 'checkbox'
+      this.pages[indexPage].questions[index].isRatingField = event.target.value === 'rating'
    }
 
 
-   sendData() {
-      // Array.from(this.questions).forEach(item =>{
-      //    console.log(item.answer, item.radioChoices)
-      // })
-      // console.log(JSON.stringify(this.questions))
-      // alert(JSON.stringify(this.questions))
-      const data = this.questions
+   // sendData() {
+   //    // Array.from(this.questions).forEach(item =>{
+   //    //    console.log(item.answer, item.radioChoices)
+   //    // })
+   //    // console.log(JSON.stringify(this.questions))
+   //    // alert(JSON.stringify(this.questions))
+   //    const data = this.questions
 
-      const dataChangeEvent = new CustomEvent('datachange', {
-         detail: data
-      })
+   //    const dataChangeEvent = new CustomEvent('datachange', {
+   //       detail: data
+   //    })
 
-      this.dispatchEvent(dataChangeEvent)
-   }
+   //    this.dispatchEvent(dataChangeEvent)
+   // }
+
+   // handleSave(event) {
+
+   // }
+   
+   
 
    handleSaveChanges(event) {
+      const indexPage = event.target.dataset.page
       const index = event.target.dataset.id
-      this.questions[index].question_text = event.target.value
+      this.pages[indexPage].questions[index].question_text = event.target.value
    }
 
    handleDeleteQuestion(event) {
+      const indexPage = event.target.dataset.page
       const index = event.target.dataset.id
-      this.questions = this.questions.filter((item, i) => i != parseInt(index))
+      this.pages[indexPage].questions = this.pages[indexPage].questions.filter((item, i) => i != parseInt(index))
    }
    
    handleAddRadio(event) {
+      const indexPage = event.target.dataset.page
       const index = event.target.dataset.id
-      this.questions[index].radioChoices.push({
+      this.pages[indexPage].questions[index].radioChoices.push({
          value: ''
       })
    }
    
    handleRadioChange(event) {
+      const indexPage = event.target.dataset.page
       const index1 = event.target.dataset.id
       const index = event.target.dataset.rad
-      this.questions[index1].radioChoices[index].value = event.target.value
+      this.pages[indexPage].questions[index1].radioChoices[index].value = event.target.value
    }
 
    handleDeleteRadio(event) {
+      const indexPage = event.target.dataset.page
       const index1 = event.target.dataset.id
       const index = event.target.dataset.rad
-      this.questions[index1].radioChoices = this.questions[index1].radioChoices.filter((item, i) => i !== parseInt(index))
+      this.pages[indexPage].questions[index1].radioChoices = this.pages[indexPage].questions[index1].radioChoices.filter((item, i) => i !== parseInt(index))
    }
 
    handleAddCheckBox(event) {
+      const indexPage = event.target.dataset.page
       const index = event.target.dataset.id
-      this.questions[index].checkBoxChoices.push({
+      this.pages[indexPage].questions[index].checkBoxChoices.push({
          value: ''
       })
    }
    
    handleCheckBoxChange(event) {
+      const indexPage = event.target.dataset.page
       const index1 = event.target.dataset.id
       const index = event.target.dataset.check
-      this.questions[index1].checkBoxChoices[index].value = event.target.value
+      this.pages[indexPage].questions[index1].checkBoxChoices[index].value = event.target.value
    }
 
    handleDeleteCheckBox(event) {
+      const indexPage = event.target.dataset.page
       const index1 = event.target.dataset.id
       const index = event.target.dataset.check
-      this.questions[index1].checkBoxChoices = this.questions[index1].checkBoxChoices.filter((item, i) => i !== parseInt(index))
+      this.pages[indexPage].questions[index1].checkBoxChoices = this.pages[indexPage].questions[index1].checkBoxChoices.filter((item, i) => i !== parseInt(index))
    }
 
    handleMinScale(event) {
+      const indexPage = event.target.dataset.page
       const index = event.target.dataset.id
-      this.questions[index].minScale = parseInt(event.target.value)
+      this.pages[indexPage].questions[index].minScale = parseInt(event.target.value)
    }
 
    handleMaxScale(event) {
+      const indexPage = event.target.dataset.page
       const index = event.target.dataset.id
-      this.questions[index].maxScale = parseInt(event.target.value)
+      this.pages[indexPage].questions[index].maxScale = parseInt(event.target.value)
    }
 
    handleMandate(event) {
+      const indexPage = event.target.dataset.page
       const index = event.target.dataset.id
-      this.questions[index].required = event.target.checked
+      this.pages[indexPage].questions[index].required = event.target.checked
    }
 
    get jsonData() {
-      return JSON.stringify(this.questions, null, 2)
+      return JSON.stringify(this.pages, null, 2)
    }
 
+   handleDeletePage(event) {
+      const indexPage = event.target.dataset.page
+      this.pages = this.pages.filter((item, i) => i !== parseInt(indexPage))
+   }
+
+   handleTitleChange(event) {
+      this.title = event.target.value
+   }
+
+   handleSave(event) {
+      const surveyData = JSON.stringify(this.pages);
+
+      createSurvey({ title: this.title, pagesJson: surveyData })
+          .then(() => {
+              this.dispatchEvent(
+                  new ShowToastEvent({
+                      title: 'Success',
+                      message: 'Survey created successfully',
+                      variant: 'success'
+                  })
+              );
+          })
+          .catch(error => {
+              this.dispatchEvent(
+                  new ShowToastEvent({
+                      title: 'Error',
+                      message: error.body.message,
+                      variant: 'error'
+                  })
+              );
+          });
+         }
    
 }
